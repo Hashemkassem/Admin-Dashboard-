89c26f411f6636b85632ee709670bee659e5fa51
Hashemkassem <mohammadhashemkassem@gmail.com>
2024-07-07 15:02:51 +0300

    first commit

---
+++ flutter_responsive/lib/screens/dashboard/dashboard_screen.dart
@@ -0,0 +1,55 @@
+// ignore_for_file: use_key_in_widget_constructors
+
+import 'package:flutter/material.dart';
+import 'package:flutter_responsive/screens/dashboard/components/my_fields.dart';
+
+import '../../constants.dart';
+import '../../responsive.dart';
+import 'components/header.dart';
+
+import 'components/recent_files.dart';
+import 'components/storage_details.dart';
+
+class DashboardScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SingleChildScrollView(
+        primary: false,
+        padding: const EdgeInsets.all(defaultPadding),
+        child: Column(
+          children: [
+            const Header(),
+            const SizedBox(height: defaultPadding),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  flex: 5,
+                  child: Column(
+                    children: [
+                      const MyFiles(),
+                      const SizedBox(height: defaultPadding),
+                      const RecentFiles(),
+                      if (Responsive.isMobile(context))
+                        const SizedBox(height: defaultPadding),
+                      if (Responsive.isMobile(context)) const StorageDetails(),
+                    ],
+                  ),
+                ),
+                if (!Responsive.isMobile(context))
+                  const SizedBox(width: defaultPadding),
+                // On Mobile means if the screen is less than 850 we don't want to show it
+                if (!Responsive.isMobile(context))
+                  const Expanded(
+                    flex: 2,
+                    child: StorageDetails(),
+                  ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
